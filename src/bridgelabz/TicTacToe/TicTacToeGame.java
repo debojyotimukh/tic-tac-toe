9f7cd24ca1848e79426c257602da058d43ee61be
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-05 09:50:15 +0530

    formatting

---
+++ src/bridgelabz/TicTacToe/TicTacToeGame.java
@@ -0,0 +1,48 @@
+package bridgelabz.TicTacToe;
+
+import java.util.Scanner;
+
+public class TicTacToeGame {
+	private static final char EMPTY = ' ';
+	private static final char CROSS = 'X';
+	private static final char ROUND = 'O';
+	private static char playerSymbol;
+	private static char computerSymbol;
+
+	private char[] board;
+
+	TicTacToeGame() {
+		board = new char[10];
+		for (int i = 0; i < board.length; i++)
+			this.board[i] = EMPTY;
+	}
+
+	private static int getIndex(int row, int col) {
+		return 3 * (row - 1) + (col - 1);
+	}
+
+	public void choosePlayerSymbol(char playerSymbol) {
+		if (playerSymbol == CROSS) {
+			this.playerSymbol = CROSS;
+			this.computerSymbol = ROUND;
+		} else if (playerSymbol == ROUND) {
+			this.playerSymbol = ROUND;
+			this.computerSymbol = CROSS;
+		} else
+			System.out.println("Invalid Symbol");
+	}
+
+	public char getPlayerSymbol() {
+		return playerSymbol;
+	}
+
+	public static void main(String[] args) {
+		TicTacToeGame game = new TicTacToeGame();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choose a symbol (X or O): ");
+		String symbol = sc.next();
+		game.choosePlayerSymbol(symbol.charAt(0));
+		System.out.println("Player has chosen: " + game.getPlayerSymbol());
+
+	}
+}
--- src/module-info.java
+++
@@ -1,2 +0,0 @@
-module bridgelabz.TicTacToe {
-}
